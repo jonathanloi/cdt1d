4f110e4ebb84d2a59b61df8cab46dec7e13b8ad6
jonathanloi <38899281+jonathanloi@users.noreply.github.com>
2021-12-06 21:04:42 +0800

    commit

--- questions.py
+++ questions.py
@@ -1,31 +1,26 @@
 class questions:
-    def __init__(self):
-        self.qn1 = """You've got homework and assignments due the next day and your friend asked you to hang out together with her. Would you ...
+    def questions(self):
+        question = {1:"""You've got homework and assignments due the next day and your friend asked you to hang out together with her. Would you ...
 
 a. join her because of peer pressure
 b. decline politely and said you would join next time
-c. None of the above"""
-
-        self.qn2 = """Upon hearing negative comments about yourself from others, how would you react?
+c. None of the above""", 2:"""Upon hearing negative comments about yourself from others, how would you react?
 
 a. Self-blame and self-hate
 b. React to it positively
-c. None of the above"""
-    
-        self.qn3 = """You see your friends post stories with other groups of friends, you...
+c. None of the above""", 3:"""You see your friends post stories with other groups of friends, you...
 
 a. wonder why you do not have as many friends as them
 b. feel happy for them
-c. None of the above"""
-    
-        self.qn4 = """Your group members are experienced in the topic for your school assignment, but you are completely new to the topic. You...
+c. None of the above""", 4:"""Your group members are experienced in the topic for your school assignment, but you are completely new to the topic. You...
 
 a. feel bad about yourself for not knowing as much as them and cannot contribute much to the project
 b. see it as an opportunity to learn from your group mates and improve yourself
-c. None of the above"""
-    
-        self.qn5="""You are out with your friends for dinner and they suggest to go for a few drinks. Based on past experience, it never stops at a few. Your friends are all trying to convince you but you have an important meeting tomorrow that could greatly affect your life. What will you do?\n
+c. None of the above""", 5:"""You are out with your friends for dinner and they suggest to go for a few drinks. Based on past experience, it never stops at a few. Your friends are all trying to convince you but you have an important meeting tomorrow that could greatly affect your life. What will you do?
 
 a. YOLO join them and get completely wasted! A little alcohol in your body will increase confidence during the meeting.
 b. Reject their bad influence and get new friends.
-c. None of the above."""  
+c. None of the above."""}
+        return question
+ed
+e)
